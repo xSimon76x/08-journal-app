a7c1e61098fba53f7e8a775e2a95919a31eda317
Simon <simonbustamante21@gmail.com>
2023-04-07 19:45:11 -0400

    Creacion de login

---
+++ src/journal/pages/JournalPage.jsx
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+export const JournalPage = () => {
+  return (
+   
+    <Typography variant='h1'>
+      JournalPage
+    </Typography>
+    
+  )
+}
