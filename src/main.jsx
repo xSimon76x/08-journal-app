c3a8e8f5936f8cecb6fbb40a3229c867f6d026c6
Simon Iván Bustamante Venegas <73963276+xSimon76x@users.noreply.github.com>
2023-04-07 13:41:16 -0400

    cambios

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { JournalApp } from './JournalApp'
 import './styles.css'
-
+// Cambios
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <JournalApp />
