f8041be7755440b485a66ce7e80d1e656eab6bc4
Simon <simonbustamante21@gmail.com>
2023-04-09 22:03:42 -0400

    Fin seccion 17 - JournalApp

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,7 +1,47 @@
 import React from 'react'
+import { AuthLayout } from '../layout/AuthLayout'
+import { Grid, TextField, Typography, Link, Button } from '@mui/material'
+import { Google } from '@mui/icons-material'
+import { Link as RouterLink } from 'react-router-dom'
 
 export const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <AuthLayout title='Crear Cuenta'>
+      <form action="">
+        <Grid container >
+          <Grid item xs={ 12 } sx={{ mt: 2 }}>
+            <TextField label='Nombre Completo' type='text' placeholder='Nombre Completo' fullWidth/>
+          </Grid>
+          <Grid item xs={ 12 } sx={{ mt: 2 }}>
+            <TextField label='Correo' type='email' placeholder='correo@gmail.com' fullWidth/>
+          </Grid>
+          <Grid item xs={ 12 } sx={{ mt: 2 }}>
+            <TextField label='Contraseña' type='password' placeholder='******' fullWidth/>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} sx={{ mb:2, mt: 2}}>
+          <Grid item xs={12} sm={6}>
+            <Button variant='contained' fullWidth>
+              Crear Cuenta
+            </Button>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Button variant='contained' fullWidth>
+              <Google />
+              <Typography sx={{ ml: 1}}> Google </Typography>
+            </Button>
+          </Grid>
+        </Grid>
+
+        <Grid container direction='row' justifyContent='end'>
+          <Typography sx={{ mr: 1 }}>Ya tienes cuenta?</Typography>
+          <Link component={ RouterLink } color='inherit' to='/auth/login'>
+            Ingresar
+          </Link>
+        </Grid>
+      
+      </form>
+    </AuthLayout>
   )
 }
---
+++ src/journal/components/SideBar.jsx
@@ -0,0 +1,55 @@
+import { TurnedInNot } from '@mui/icons-material'
+import { Box, Divider, Drawer, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material'
+import React from 'react'
+
+export const SideBar = ({ drawerWidth = 24 }) => {
+  return (
+    <Box
+        component='nav'
+        sx={{
+            width: { sm: drawerWidth }, flexShrink: { sm: 0 }
+        }}
+    >
+        <Drawer
+            variant='permanent'
+            open
+            sx={{
+                display: { xs: 'block' },
+                '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth }
+            }}
+        >
+            <Toolbar>
+                <Typography
+                    variant='h6'
+                    noWrap
+                    component='div'
+                >
+                    Simon Bustamante
+                </Typography>
+            </Toolbar>
+            <Divider />
+
+            <List>
+                {
+                    ['Enero','Febrero','Marzo','Abril','Mayo'].map( item => (
+                        <ListItem key={item} disablePadding>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <TurnedInNot />
+                                </ListItemIcon>
+                                <Grid container>
+                                    <ListItemText primary={item} />
+
+                                    <ListItemText secondary={ 'lorem lorem lorem loremlorem lorem loremloremlorems' }/>
+                                </Grid>
+                            </ListItemButton>
+                        </ListItem>
+                    ))
+                }
+            </List>
+
+
+        </Drawer>
+    </Box>
+  )
+}
