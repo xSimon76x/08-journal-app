40502c7d9fcd003148579f6a0c265baccbaf2194
Simon <simonbustamante21@gmail.com>
2023-04-30 22:15:17 -0400

    funcion de activar una nota

--- src/journal/components/SideBar.jsx
+++ src/journal/components/SideBar.jsx
@@ -2,58 +2,50 @@ import { TurnedInNot } from '@mui/icons-material'
 import { Box, Divider, Drawer, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material'
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { SideBarItem } from './SideBarItem'
 
 export const SideBar = ({ drawerWidth = 24 }) => {
 
     const { displayName } = useSelector( state => state.auth );
 
-  return (
-    <Box
-        component='nav'
-        sx={{
-            width: { sm: drawerWidth }, flexShrink: { sm: 0 }
-        }}
-    >
-        <Drawer
-            variant='permanent'
-            open
+    const { notes } = useSelector( state => state.journal );
+
+    return (
+        <Box
+            component='nav'
             sx={{
-                display: { xs: 'block' },
-                '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth }
+                width: { sm: drawerWidth }, flexShrink: { sm: 0 }
             }}
         >
-            <Toolbar>
-                <Typography
-                    variant='h6'
-                    noWrap
-                    component='div'
-                >
-                    { displayName }
-                </Typography>
-            </Toolbar>
-            <Divider />
+            <Drawer
+                variant='permanent'
+                open
+                sx={{
+                    display: { xs: 'block' },
+                    '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth }
+                }}
+            >
+                <Toolbar>
+                    <Typography
+                        variant='h6'
+                        noWrap
+                        component='div'
+                    >
+                        { displayName }
+                    </Typography>
+                </Toolbar>
+                <Divider />
 
-            <List>
-                {
-                    ['Enero','Febrero','Marzo','Abril','Mayo'].map( item => (
-                        <ListItem key={item} disablePadding>
-                            <ListItemButton>
-                                <ListItemIcon>
-                                    <TurnedInNot />
-                                </ListItemIcon>
-                                <Grid container>
-                                    <ListItemText primary={item} />
-
-                                    <ListItemText secondary={ 'lorem lorem lorem loremlorem lorem loremloremlorems' }/>
-                                </Grid>
-                            </ListItemButton>
-                        </ListItem>
-                    ))
-                }
-            </List>
+                <List>
+                    {
+                        notes.map( note => (
+                            <SideBarItem key={ note.id } { ...note } />
+                        ))
+                    }
+                </List>
 
 
-        </Drawer>
-    </Box>
-  )
+            </Drawer>
+        </Box>
+    )
 }
---
+++ src/journal/components/SideBarItem.jsx
@@ -0,0 +1,46 @@
+import { TurnedInNot } from "@mui/icons-material"
+import { Grid, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material"
+import { useMemo } from "react"
+import { useDispatch } from "react-redux"
+import { setActiveNote } from "../../store/Journal"
+
+
+export const SideBarItem = ({ title, body, id, imageUrls = [] }) => {
+
+    const dispatch = useDispatch();
+
+    const newTitle = useMemo( 
+        () => {
+            return title.length > 17 
+                ? title.substring(0, 17) + '...'
+                : title;
+        }, [ title ]
+    )
+
+    const onClickActiveNote = () => {
+
+        const noteActive = {
+            title,
+            body,
+            id,
+            imageUrls
+        }
+
+        dispatch( setActiveNote( noteActive ) );
+    }
+
+    return (
+        <ListItem  disablePadding onClick={ onClickActiveNote }>
+            <ListItemButton>
+                <ListItemIcon>
+                    <TurnedInNot />
+                </ListItemIcon>
+                <Grid container>
+                    <ListItemText primary={ newTitle } />
+
+                    <ListItemText secondary={ body }/>
+                </Grid>
+            </ListItemButton>
+        </ListItem>
+    )
+}
