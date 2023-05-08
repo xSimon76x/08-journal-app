fe155b971d01b0bf82fdfe2691f5d3e55026fed9
Simon <simonbustamante21@gmail.com>
2023-05-07 23:32:28 -0400

    Pruebas en el thunks 'checkingAuthentication'

---
+++ test/store/auth/thunks.test.js
@@ -0,0 +1,21 @@
+import { checkingCredentials } from "../../../src/store/auth";
+import { checkingAuthentication } from "../../../src/store/auth/thunks";
+
+jest.mock('../../../src/firebase/providers');
+
+describe('Pruebas en AuthThunks', () => {
+    const dispatch = jest.fn();
+
+    beforeEach(
+        () => jest.clearAllMocks()
+    );
+  
+    test('debe de invocar el checkingCredentials', async() => {
+
+        await checkingAuthentication()( dispatch );
+
+        expect( dispatch ).toHaveBeenCalledWith( checkingCredentials() );
+    });
+    
+
+})
